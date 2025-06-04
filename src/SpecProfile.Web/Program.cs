d802d4db64c7bd68c7e0224761caa44fa9272874
Mihucza, Bence <mihben@gmail.com>
2025-06-04 12:32:11 +0200

    chore: Integrated serilog

--- src/SpecProfile.Web/Program.cs
+++ src/SpecProfile.Web/Program.cs
@@ -1,6 +1,10 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Host.UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration));
 
 builder.Services.AddControllers();
 
