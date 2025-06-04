feaf85021c148d42ec2fa2f1cc91f5772a63ab90
Mihucza, Bence <mihben@gmail.com>
2025-06-04 13:50:01 +0200

    chore: Integrated S-Train

--- src/SpecProfile.Web/Program.cs
+++ src/SpecProfile.Web/Program.cs
@@ -1,12 +1,17 @@
 using Serilog;
+using SpecProfile.Web.Wireups;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Host.UseLightInject();
 builder.Host.UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration));
 
 builder.Services.AddControllers();
+
+builder.UseCQS();
+builder.UseEventing();
 
 var app = builder.Build();
 
---
+++ src/SpecProfile.Web/Wireups/Wireups.cs
@@ -0,0 +1,26 @@
+﻿using STrain.CQS.NetCore;
+using STrain.CQS.NetCore.Builders;
+using STrain.Eventing.KurrentDB.NetCore.Extensions;
+
+namespace SpecProfile.Web.Wireups
+{
+	public static class Wireups
+	{
+		public static void UseCQS(this WebApplicationBuilder builder)
+		{
+			builder.AddCQS(builder =>
+			{
+				builder.AddGenericRequestHandler()
+				.AddMvcRequestReceiver();
+
+				builder.AddRequestValidator()
+					.UseFluentRequestValidator(builder => { });
+			});
+		}
+
+		public static void UseEventing(this WebApplicationBuilder builder)
+		{
+			builder.AddEventing(builder => builder.AddKurrentDB((settings, configuration) => configuration.Bind("KurrentDB", settings)));
+		}
+	}
+}
